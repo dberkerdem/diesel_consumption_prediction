407cab5da33ccf7157a48926995605f887442a0b
dberkerdem <dberkerdem@outlook.com>
2022-09-21 05:23:46 +0300

    Scoring Completed

---
+++ src/model_selection/gbtrees.py
@@ -0,0 +1,70 @@
+from abc import ABC, abstractmethod
+import lightgbm as lgb
+import xgboost as xgb
+import catboost as cbt
+import pandas as pd
+from typing import Any
+
+class BaseEstimator(ABC):
+    """BaseEstimator is the abstract class for Statistical/ML/DeepLearning models.
+    """
+    def __init__(self, estimator_list: list,**kwargs):
+        """Default Constructor
+        """
+        self.estimator_list = estimator_list
+        pass
+        
+class GBTrees(BaseEstimator):
+    def __init__(self, estimator_list: list, **kwargs):
+        """Default Constructor
+        """
+        super().__init__(estimator_list=estimator_list,**kwargs)
+        self.score_: Any
+        pass
+    @property
+    def prediction_scores(self)->pd.DataFrame:
+        return self.score_
+            
+    def tree_init_pipeline_(self, **kwargs):
+        estimator_init_pipeline = dict()
+        if "xgboost" in self.estimator_list:
+            estimator_init_pipeline["xgboost"] = xgb.XGBRegressor(**kwargs["init_params"]["xgboost__init_params"])
+            # print(kwargs["xgboost__init_params"])
+        if "lightgbm" in self.estimator_list:
+            estimator_init_pipeline["lightgbm"] = lgb.LGBMRegressor(**kwargs["init_params"]["lightgbm__init_params"])
+            # print(kwargs["lightgbm__init_params"])
+        if "catboost" in self.estimator_list:
+            estimator_init_pipeline["catboost"] = cbt.CatBoostRegressor(**kwargs["init_params"]["catboost__init_params"])
+            # print(kwargs["catboost__init_params"])
+        return estimator_init_pipeline
+    
+    def tree_fit_pipeline_(self, X_train: pd.DataFrame, y_train: pd.DataFrame, hyper_params:dict):
+        estimator_fit_pipeline = dict()
+        # Init Trees
+        estimator_init_pipeline = self.tree_init_pipeline_(**hyper_params)
+        # Fit Trees
+        for est in self.estimator_list:
+            estimator_fit_pipeline[est] = estimator_init_pipeline[est].fit(X_train, y_train,)
+        return estimator_fit_pipeline
+            
+    def tree_predict_pipeline_(self, X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, 
+                               y_test: pd.DataFrame, hyper_params:dict, run: int):
+        estimator_predict_pipeline = y_test.reset_index(level=["date"],drop=True).copy()
+        fitted_gbtrees = self.tree_fit_pipeline_(X_train=X_train, y_train=y_train, hyper_params=hyper_params)
+        for est in self.estimator_list:
+            estimator_predict_pipeline[f"{est}_pred"] = fitted_gbtrees[est].predict(X_test)
+        self._prediction_scores_(predictions=estimator_predict_pipeline, run=run)
+        return estimator_predict_pipeline
+        
+    def ape(self, actual_value: Any, prediction: Any)->float:
+        ape_score = abs(actual_value-prediction)/actual_value*100
+        return 1/ape_score
+
+    def _prediction_scores_(self, predictions: pd.DataFrame, run: int, target_value: str="current_month_consumption")-> pd.DataFrame:
+        temp_df = predictions.copy()
+        col_list = []
+        for est in self.estimator_list:
+            # Add percentage difference
+            temp_df[f"{est}_{run}_APE"] = temp_df.apply(lambda row: self.ape(row[target_value], row[f"{est}_pred"]), axis=1)
+            col_list.append(f"{est}_{run}_APE")
+        self.score_ = temp_df[col_list]
--- src/model_selection/hparam_opt.py
+++ src/model_selection/hparam_opt.py
@@ -1,12 +1,8 @@
 from abc import ABC, abstractmethod
 from typing import Any
 import pandas as pd
-import numpy as np
-import lightgbm as lgb
-import xgboost as xgb
-import catboost as cbt
 from sklearn.model_selection import TimeSeriesSplit, GridSearchCV, RandomizedSearchCV
-from src.model_selection.data_preperation import DataPreperation as dp
+from src.model_selection.gbtrees  import GBTrees
 
 class BaseEstimator(ABC):
     """BaseEstimator is the abstract class for Statistical/ML/DeepLearning models.
@@ -14,56 +10,29 @@ class BaseEstimator(ABC):
     def __init__(self, **kwargs):
         """Default Constructor
         """
-        
-        pass
-
-class GBTrees(BaseEstimator):
-    def __init__(self, **kwargs):
-        """Default Constructor
-        """
-        pass
-    @staticmethod
-    def make_estimator_pipeline_(estimator_list: list, **kwargs):
-        estimator_pipeline = dict()
-        print("Estimators are:",estimator_list)
-        # print(kwargs)
-        if "xgboost" in estimator_list:
-            estimator_pipeline["xgboost"] = xgb.XGBRegressor(**kwargs["xgboost__init_params"])
-            # print(kwargs["xgboost__init_params"])
-        if "lightgbm" in estimator_list:
-            estimator_pipeline["lightgbm"] = lgb.LGBMRegressor(**kwargs["lightgbm__init_params"])
-            # print(kwargs["lightgbm__init_params"])
-        if "catboost" in estimator_list:
-            estimator_pipeline["catboost"] = cbt.CatBoostRegressor(**kwargs["catboost__init_params"])
-            # print(kwargs["catboost__init_params"])
-        return estimator_pipeline
-    
-    
-    
+        pass    
     
 class HyparamOptimizer(BaseEstimator):
     def __init__(self, **kwargs):
         """Default Constructor.
         """
         super().__init__(**kwargs)
-    
-    @staticmethod
-    def optimize_hyparams(X_train: pd.DataFrame, y_train: pd.DataFrame, optimization_config: dict):        
-        best_models_and_params = HyparamOptimizer().optimizer_pipeline_(X_train=X_train, y_train=y_train, 
-                                                                      **optimization_config)
+
+    def optimize_hyparams(self, X_train: pd.DataFrame, y_train: pd.DataFrame, estimator_list: list, optimization_params: dict):        
+        best_models_and_params = self.optimizer_pipeline_(X_train=X_train, y_train=y_train,
+                                                                        estimator_list=estimator_list, **optimization_params)
         return best_models_and_params
 
     def optimizer_pipeline_(self, X_train: pd.DataFrame, y_train: pd.DataFrame,
                             estimator_list: list, estimator_params: dict, **kwargs):
         # Establish Estimators pipeline
-        estimator_pipeline = GBTrees().make_estimator_pipeline_(estimator_list=estimator_list, **estimator_params["init_params"])
+        estimator_pipeline = GBTrees(estimator_list=estimator_list,).tree_init_pipeline_(**estimator_params)
         # Initialize the Cross Validator
         cv = self.get_cross_validator(**kwargs["cross_validation_params"])
-        # Initialize an dictionary for optimized model:hyparameter pairs
-        best_models_params = dict()
+        # Initialize an dictionary for optimized hyparameters pairs
+        optimal_params = {"init_params":{}}
         # Perform Optimization and Store Results
         for est in estimator_list:
-            print("Estimator is",est)
             # Alttaki satırı sil
             self.calc_grid_pairs(estimator_params["grid_search_params"][f"{est}__grid_search_params"])        
             grid = self.hp_optimizer(
@@ -73,9 +42,10 @@ class HyparamOptimizer(BaseEstimator):
                 **kwargs["hp_optimizer"]
                 )
             grid.fit(X=X_train, y=y_train, **estimator_params["fit_params"][f"{est}__fit_params"])
-            best_models_params[est] = [grid.best_estimator_,grid.best_params_]
+            optimal_params["init_params"][f"{est}__init_params"] = grid.best_params_
+            # best_models_params[est] = [grid.best_estimator_,grid.best_params_]
             print(f"R-S for {est} is done.")
-        return best_models_params
+        return optimal_params
         
     def hp_optimizer(self, estimator: Any, search_params:dict, cv:Any, optimizer_type:str, scoring:str, n_jobs:int):
         if "GridSearchCV" in optimizer_type:
@@ -99,8 +69,6 @@ class HyparamOptimizer(BaseEstimator):
                         
     def get_cross_validator(self, validator: str, k: int=9, test_size: int=81, gap: int=0):
         if validator == "time_series":
-            from sklearn.model_selection import TimeSeriesSplit
-            # print("time_series_cross_val")
             tscv = TimeSeriesSplit(gap=gap, max_train_size=None, n_splits=k, test_size=test_size)
             return tscv
         else:
